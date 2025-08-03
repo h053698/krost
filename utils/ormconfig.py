6e3ebc76c7c7c6f2e3aeddab54766c4e724fb1f3
Yuchan Han <yuchanhan71@gmail.com>
2025-08-04 02:07:41 +0900

    Add ORM configuration for PostgreSQL database and User entity definition

---
+++ utils/ormconfig.py
@@ -0,0 +1,26 @@
+from pony.orm import Database, PrimaryKey, Required, Optional
+from utils.env_validator import get_settings
+
+__all__ = ["database"]
+
+settings = get_settings()
+
+database = Database()
+database.bind(
+    provider="postgres",
+    user=settings.DATABASE_USER,
+    password=settings.DATABASE_PASSWORD,
+    host=settings.DATABASE_HOST,
+    database=settings.DATABASE_NAME,
+)
+
+
+class User(database.Entity):
+    id = PrimaryKey(str)
+    username = Required(str, unique=True)
+    credential_id = Optional(str)
+    public_key = Optional(str)
+    sign_count = Optional(int, default=0)
+
+
+database.generate_mapping(create_tables=True)
