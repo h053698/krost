cc06c17f4c1360d3014decf7527fac14d70adda5
Yuchan Han <yuchanhan71@gmail.com>
2025-08-06 04:40:44 +0900

    Add 'extra' option to Settings model configuration for improved flexibility

--- utils/env_validator.py
+++ utils/env_validator.py
@@ -5,7 +5,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", case_sensitive=True
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=True,
+        extra="ignore"
     )
     DATABASE_HOST: str = "localhost"
     DATABASE_PORT: int = 5432
